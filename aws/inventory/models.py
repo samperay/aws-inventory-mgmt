243223f1384ba5e771c62ca3dadd0278254ab899
samperay <sunlnx@gmail.com>
2020-01-15 12:20:27 +0530

    added model views

--- aws/inventory/models.py
+++ aws/inventory/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Invs(models.Model):
+    instance_id = models.CharField(max_length=30)
+    instance_type = models.CharField(max_length=30)
+    ami_id = models.CharField(max_length=30)
+    instance_status = models.CharField(max_length=30)
+    availability_zone = models.CharField(max_length=30)
+    public_ip = models.CharField(max_length=30)
+    private_ip = models.CharField(max_length=30)
+    instance_keypair = models.CharField(max_length=30)
+    dns_public = models.CharField(max_length=30)
+    dns_private = models.CharField(max_length=30)
+
+    def __str__(self):
+        return str.name
