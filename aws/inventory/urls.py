e2267a8909949edb201edc7c7edaca9df2e7eded
samperay <sunlnx@gmail.com>
2020-01-15 12:51:26 +0530

    search modules added

--- aws/inventory/urls.py
+++ aws/inventory/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name = 'inventory-home' ),
     path('getdata/', views.getdata, name = 'inventory-getdata'),
+    path('search/', views.search, name = 'inventory-search'),
 ]
--- aws/inventory/views.py
+++ aws/inventory/views.py
@@ -1,22 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-import django.db
-import csv
-
-reader  = csv.DictReader(open('inventory.csv', 'r'))
-inventories = []
-for line in reader:
-    inventories.append(line)
-
-info = inventories
-
+from django.http import Http404
+from .models import Invs
 
 def home(request):
     return render(request, 'inventory/home.html')
 
 def getdata(request):
-
+    info = Invs.objects.all()
     context = {
         'info': info
         }
     return render(request, 'inventory/getdata.html', context)
+
+def search(request,availability_zone):
+    try:
+        info = Invs.objects.get(availability_zone=availability_zone)
+    except Invs.DoesNotExist:
+        raise Http404('availability_zone not found')
+    return render(request, 'inventory/search.html', {'info': info})
